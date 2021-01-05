3fe8fc51dd8a8469e1e26d3822c7fedc9b417ca5
yuffy-tech <gakuen114@gmail.com>
2021-01-05 18:14:08 +0900

    ユーザー編集機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_12_25_044607) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
